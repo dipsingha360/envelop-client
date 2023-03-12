eb793e04fd66e516d49c400960ce688bff305961
Dip Singha <dipsingha2@gmail.com>
2023-03-12 15:20:33 +0600

    login added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/Button";
+import FormControl from "../components/FormControl";
+import SectionTitle from "../components/SectionTitle";
+
 const Login = () => {
-  return <div>Login</div>;
+  // states
+  const [formFields, setFormFields] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(formFields);
+
+    // clear
+    setFormFields({
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div className="login flex flex-col justify-center items-center mt-20">
+      <form onSubmit={handleLogin} className="flex flex-col gap-5">
+        <SectionTitle title={"Login"} />
+
+        <FormControl
+          label="email"
+          innerLabel="Email address"
+          inputType="email"
+          placeholder="Write your email"
+          formFields={formFields}
+          setFormFields={setFormFields}
+        />
+
+        <FormControl
+          label="password"
+          innerLabel="Password"
+          inputType="password"
+          placeholder="Write your password"
+          formFields={formFields}
+          setFormFields={setFormFields}
+        />
+        <Button submit="submit" innerLabel="Login" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
