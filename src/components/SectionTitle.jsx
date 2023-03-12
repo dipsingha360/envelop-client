45185a7598cfe80afec9b330bcef55ac61e64a81
Dip Singha <dipsingha2@gmail.com>
2023-03-12 14:40:02 +0600

    form added

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SectionTitle = ({ title }) => {
+  return (
+    <h2 className="secetion-title text-3xl font-semibold text-violet-500">
+      {title}
+    </h2>
+  );
+};
+
+export default SectionTitle;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import SectionTitle from "../components/SectionTitle";
 const Register = () => {
-  return <div>Register</div>;
+  // states
+  const [formFields, setFormFields] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="register flex flex-col justify-center items-center">
+      <form onSubmit={handleRegister} className="flex flex-col gap-5">
+        <SectionTitle title={"Register account"} />
+        <div className="form-control flex flex-col gap-2">
+          <label htmlFor="name" className=" cursor-pointer">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Your name"
+            value={formFields.name}
+            onChange={(e) =>
+              setFormFields({ ...formFields, name: e.target.value })
+            }
+            className="border px-5 py-3 rounded-lg outline-none focus:border-violet-500 w-[25rem]"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
